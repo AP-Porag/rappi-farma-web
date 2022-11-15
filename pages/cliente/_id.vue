ccc99162e2d7f6e381aeb5c779329806a2cff690
main12sani <main12sani@gmail.com>
2022-11-15 23:25:59 +0600

    profile

--- pages/cliente/_id.vue
+++ pages/cliente/_id.vue
@@ -6,16 +6,26 @@
           <div class="col-lg-4">
             <div class="ms__customer--avatar">
               <div class="ms__img">
-                <img data-src="/images/profile-cover.png" class="img-fluid" alt="" v-lazy-load>
+                <img
+                  data-src="/images/profile-cover.png"
+                  class="img-fluid"
+                  alt=""
+                  v-lazy-load
+                />
               </div>
               <div class="ms__avatar--profiles">
-                <img data-src="/images/profile-pic.png" class="img-fluid" alt="" v-lazy-load>
+                <img
+                  data-src="/images/profile-pic.png"
+                  class="img-fluid"
+                  alt=""
+                  v-lazy-load
+                />
                 <h5>Alex Mitchell</h5>
                 <p>alexmitchell@gmail.com</p>
               </div>
               <ul class="customer-nav">
                 <li>
-                  <NuxtLink to="#">Basic Info</NuxtLink>
+                  <NuxtLink to="#" class="active">Basic Info</NuxtLink>
                 </li>
                 <li>
                   <NuxtLink to="#"> Update Profile</NuxtLink>
@@ -28,6 +38,66 @@
                 </li>
               </ul>
             </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="ms__customer--content">
+                <h4>Personal Info</h4>
+                <form action="#">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label for="#">First name</label>
+                        <input
+                          type="text"
+                          placeholder="First name"
+                          class="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label for="#">Last name</label>
+                        <input
+                          type="text"
+                          placeholder="Last name"
+                          class="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label for="#">Role</label>
+                        <input
+                          type="text"
+                          placeholder="Role"
+                          class="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label for="#">Email</label>
+                        <input
+                          type="email"
+                          placeholder="Email"
+                          class="form-control"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label for="#">Phone Number</label>
+                        <input
+                          type="number"
+                          placeholder="Email"
+                          class="form-control"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <button class="ic-btn">Submit</button>
+                </form>
+              </div>
           </div>
         </div>
       </div>
@@ -45,7 +115,8 @@ export default {
 .ms__customer--avatar {
   box-shadow: 0px 4px 20px rgba(23, 23, 23, 0.05);
   border-radius: 6px;
-  .ms__img{
+  background-color: #fff;
+  .ms__img {
     border-radius: 6px 6px 0 0;
   }
 
@@ -53,7 +124,7 @@ export default {
     margin-top: -55px;
     text-align: center;
     padding-bottom: 25px;
-    border-bottom: 1px solid #EAEFF3;
+    border-bottom: 1px solid #eaeff3;
     img {
       width: 110px;
       height: 110px;
@@ -80,8 +151,23 @@ export default {
           color: $primary;
           background-color: rgba(65, 144, 222, 0.103);
         }
+        &.active {
+          color: $primary;
+          background-color: rgba(65, 144, 222, 0.103);
+        }
       }
     }
+  }
+}
+
+.ms__customer--content {
+  box-shadow: 0px 4px 20px rgba(23, 23, 23, 0.05);
+  background-color: #fff;
+  padding: 30px;
+  border-radius: 6px;
+  height: 100%;
+  h4 {
+    padding-bottom: 20px;
   }
 }
 </style>
--- pages/verificar/index.vue
+++ pages/verificar/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="root">
-    <h3>Checkout page</h3>
+    <section class="ms__checkout--part">
+      <div class="container">
+        <form action="#">
+          <div class="ms__card">
+            <div class="ms__title">
+              <h4 class="title">Personal Details</h4>
+            </div>
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="from-group">
+                  <label for="#">First Name</label>
+                  <input type="text" placeholder="First Name" class="form-control">
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -11,5 +29,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
