4db38f145bbf99a914cc9306c695beaae018f0ab
main12sani <main12sani@gmail.com>
2022-11-28 00:35:39 +0600

    blog page & search result

--- components/partials/Header.vue
+++ components/partials/Header.vue
@@ -13,6 +13,86 @@
             <!-- <button>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
             </button> -->
+            <div class="ms__header--searchBar--dropdwon">
+              <ul>
+                <li>
+                  <NuxtLink to="#"> Vistos recientemente</NuxtLink>
+                </li>
+                <li>
+                  <NuxtLink to="#"> Vistos recientemente</NuxtLink>
+                </li>
+              </ul>
+              <div class="ms__recent-product">
+                <ul>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                  <li>
+                    <NuxtLink to="#">
+                      <img src="https://lh3.googleusercontent.com/szQPXjkxkjIFo0BGWae2q1b_6riNQwgn5CnrKd-oQek3EGLYAh0Syz5Ha5wUKBX6Vn3KSvpiqMnWTjhVLEfdypFjXcx7bOz2YU-8Ad7mWkpKaFAAHw" class="img-fluid" alt="" v-lazy-load>
+                      <p> Hasta 35% en Dermo con Prime </p>
+                    </NuxtLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div class="ms__header--loginWith--cart">
               <a href="#" class="ic-btn-outline">
@@ -203,7 +283,7 @@
     </header>
     <!-- cart start -->
     <div class="ic__shopping--overlay"></div>
-    <div class="ic__shopping--cart">
+    <div class="ic__shopping--cart active">
         <div class="ic__shopping--top">
             <a href="#" class="ic__cart--closed">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
@@ -320,6 +400,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.ms__recent-product {
+  overflow-x: auto;
+  ul {
+    display: flex;
+    gap: 10px;
+    li {
+      flex: 0 0 100px;
+      border: 1px solid #f9f9f9;
+      border-radius: 4px;
+      p {
+        font-size: 10px;
+        padding: 8px;
+        color: #000;
+      }
+    }
+  }
+}
 .ms__header--part {
   padding: 10px 0;
 }
@@ -730,5 +827,21 @@ export default {
     }
   }
 }
-
+.ms__header--searchBar--dropdwon {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0 0 0.7rem 0 rgba(0, 0, 0, 0.199);
+  border-radius: 8px;
+  z-index: 99;
+  ul {
+    li {
+      a {
+        font-size: 14px;
+      }
+    }
+  }
+}
 </style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,239 @@
 <template>
   <div class="root">
-    <h3>blog main page</h3>
+    <!-- =============== category part start =============== -->
+<section class="ic-category-main-part ic-section-space-top-80 ic-section-space-bottom">
+    <div class="container">
+        <!-- blog -->
+        <div class="row">
+            <div class="col-lg-8 col-xl-9 col-md-8">
+                <div class="row g-xl-5 g-4">
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                    <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    <span>Table & chair</span>
+                                    <h6 class="sub">The Best Products That Office Reconfigure </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <a href="#">
+                            <div class="ic__blog__main--items">
+                                <div class="ic__blog--images">
+                                  <img class="img-fluid w-100" data-src="https://lh3.googleusercontent.com/1zIL5WuEzvujcx6jTTSffMRX6kclfb2390aXj4C0NY5gEdWBshD6f1oQgdTP4kaTAmwF379UAuye2DzOmlUcB-5LRNcNBozY0xZbU0Cc4ZDrsl0O=s650-rw" alt="images" v-lazy-load>
+                                </div>
+                                <div class="ic__blog--title">
+                                    <h6 class="sub">The best deals to shop from the sale </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xl-3 col-md-4">
+                <div class="ic-blog-search">
+                    <form action="#">
+                        <div class="position-relative">
+                            <input type="text" placeholder="Search" class="form-control">
+                            <button>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div class="ic-blog-right-card">
+                    <div class="ic-blog-right-header">
+                        <h6 class="sub">Category</h6>
+                    </div>
+                    <div class="ic-blog-card-body">
+                        <ul class="ic-blog-right-category">
+                            <li>
+                                <a href="#">Sofas & sectionals</a>
+                            </li>
+                            <li>
+                                <a href="#">Tables & Desks</a>
+                            </li>
+                            <li>
+                                <a href="#">Chairs</a>
+                            </li>
+                            <li>
+                                <a href="#">Refinishing</a>
+                            </li>
+                            <li>
+                                <a href="#">furniture</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="ic-blog-right-card">
+                    <div class="ic-blog-right-header">
+                        <h6 class="sub">Recent Post</h6>
+                    </div>
+                    <div class="ic-blog-card-body">
+                        <ul class="ic-blog-right-recent-post">
+                            <li>
+                                <a href="#" class="ic-recent-post-items">
+                                    <img class="img-fluid w-100" src="assets/images/recent-post-1.png" alt="images">
+                                    <div class="ic-recent-post-right-side-content">
+                                        <span>14 May 2021</span>
+                                        <p class="gray-800">Sunset Sets From Saks</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="ic-recent-post-items">
+                                    <img class="img-fluid w-100" src="assets/images/recent-post-2.png" alt="images">
+                                    <div class="ic-recent-post-right-side-content">
+                                        <span>16 May 2021</span>
+                                        <p class="gray-800">Process of giving a meaning</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="ic-recent-post-items">
+                                    <img class="img-fluid w-100" src="assets/images/recent-post-3.png" alt="images">
+                                    <div class="ic-recent-post-right-side-content">
+                                        <span>18 May 2021</span>
+                                        <p class="gray-800">Red Chair Almonds   </p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="ic-recent-post-items">
+                                    <img class="img-fluid w-100" src="assets/images/recent-post-4.png" alt="images">
+                                    <div class="ic-recent-post-right-side-content">
+                                        <span>20 May 2021</span>
+                                        <p class="gray-800">Promise to your customer</p>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="ic-blog-right-card">
+                    <div class="ic-blog-right-header">
+                        <h6 class="sub">Popular Tags</h6>
+                    </div>
+                    <div class="ic-blog-card-body">
+                        <ul class="ic-blog-right-popular-tag">
+                            <li>
+                                <button>Benifit</button>
+                            </li>
+                            <li>
+                                <button>Finance</button>
+                            </li>
+                            <li>
+                                <button>Digital</button>
+                            </li>
+                            <li>
+                                <button>Tax Director</button>
+                            </li>
+                            <li>
+                                <button>Money</button>
+                            </li>
+                            <li>
+                                <button>Hats</button>
+                            </li>
+                            <li>
+                                <button>Developer</button>
+                            </li>
+                            <li>
+                                <button>Socks</button>
+                            </li>
+                            <li>
+                                <button>Analyst</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- =============== category part end =============== -->
+
   </div>
 </template>
 
@@ -10,6 +243,211 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.ic__blog__main--items {
+    .ic__blog--images {
+        @include bd-radius(16px);
+        overflow: hidden;
+        box-shadow: $shadow-1;
+        img {
+            @include transition(.8s);
+            @include bd-radius(16px);
+        }
+    }
+
+    &:hover {
+        img {
+            transform: scale(1.2);
+        }
+    }
+
+    .ic__blog--title {
+        padding-top: 15px;
+
+        span {
+            color: $shuttle-gray;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 24px;
+        }
+
+        h6 {
+            font-weight: 600;
+            font-size: 20px;
+            color: $ebony-clay;
+            padding-top: 8px;
+            @media #{$max1199} {
+                font-size: 16px;
+                line-height: 24px;
+            }
+        }
+    }
+}
+
+.ic-blog-search {
+    margin-bottom: 40px;
+
+    @media #{$max767} {
+        margin-top: 40px;
+    }
+
+    .form-control {
+        height: 44px;
+        border-color: $gray-300;
+        color: $ebony-clay;
+        font-weight: 500;
+        @include bd-radius(8px);
+    }
+
+    button {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100%;
+        width: 54px;
+        background: $primary;
+        color: $shuttle-gray;
+        @include bd-radius2(0px, 8px, 8px, 0px);
+        border: none;
+        svg {
+          width: 20px;
+        }
+    }
+}
+
+.ic-blog-right-card {
+    background-color: #FFFFFF;
+    border: 1px solid $gray-300;
+    @include bd-radius(16px);
+    padding: 20px 28px;
+
+    &:not(last-child) {
+        margin-bottom: 30px;
+    }
+
+    .ic-blog-right-header {
+        border-bottom: 1px solid $gray-300;
+        padding-bottom: 10px;
+    }
+}
+
+.ic-blog-card-body {
+    padding-top: 18px;
+}
+
+.ic-blog-right-category {
+    li {
+        &:not(:last-child) {
+            padding-bottom: 16px;
+        }
+
+        a {
+            color: $gray-600;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 26px;
+            @include transition(.4s);
+            display: block;
+
+            &:hover {
+                color: $primary;
+            }
+        }
+    }
+}
+
+.ic-blog-right-recent-post {
+    li {
+        &:not(:last-child) {
+            padding-bottom: 18px;
+            border-bottom: 1px solid $gray-300;
+        }
+
+        &:not(:first-child) {
+            padding-top: 18px;
+        }
+
+        a {
+            &.ic-recent-post-items {
+                display: grid;
+                grid-template-columns: 77px 1fr;
+                column-gap: 15px;
+
+                @media #{$max991} {
+                    grid-template-columns: 50px 1fr;
+                }
+
+                img {
+                    width: 77px;
+                    @include bd-radius(8px);
+
+                    @media #{$max991} {
+                        width: 50px;
+                    }
+                }
+
+                .ic-recent-post-right-side-content {
+                    span {
+                        font-weight: 400;
+                        font-size: 14px;
+                        line-height: 18px;
+                        color: $gray-600;
+                        padding-bottom: 6px;
+                        display: inline-block;
+                    }
+
+                    p {
+                        font-weight: 500;
+                        
+                        line-height: 20px;
+                    }
+                }
+            }
+        }
+    }
+}
+
+.ic-blog-right-popular-tag {
+    @include d-flex;
+    gap: 10px;
+
+    li {
+        button {
+            padding: 4px 12px;
+            @include bd-radius(4px);
+            background-color: $gray-100;
+            font-size: 14px;
+            line-height: 20px;
+            z-index: 99;
+            position: relative;
+            @include transition(.3s);
+
+            &::after {
+                content: '';
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 0;
+                height: 100%;
+                background: $primary;
+                z-index: -1;
+                opacity: 0;
+                @include bd-radius(4px);
+                @include transition(.8s);
+                visibility: hidden;
+            }
+
+            &:hover {
+                &::after {
+                    width: 100%;
+                    opacity: 1;
+                    visibility: visible;
+                }
+
+                color: #fff;
+            }
+        }
+    }
+}
 
 </style>
